9994fc2669f88eade4a21aa1bb76148854516445
Zidane Innis <zidaneinnis@gmail.com>
2023-05-13 00:26:49 -0500

    Generating .pdf files with order data

--- app.js
+++ app.js
@@ -20,14 +20,14 @@ const store = new MongoDBStore({
   uri: MONGODB_URI,
   collection: "sessions",
 });
-const csrfProtection = csrf();
+const csrfProtection = csrf({});
 
 const fileStorage = multer.diskStorage({
   destination: (req, file, cb) => {
     cb(null, "images");
   },
   filename: (req, file, cb) => {
-    cb(null, new Date().toISOString() + "-" + file.originalname);
+    cb(null, file.filename + "-" + file.originalname);
   },
 });
 
@@ -52,7 +52,11 @@ const authRoutes = require("./routes/auth");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(
-  multer({ storage: fileStorage, fileFilter: fileFilter }).single("image")
+  multer({
+    dest: "images",
+    storage: fileStorage,
+    fileFilter: fileFilter,
+  }).single("image")
 );
 app.use(express.static(path.join(__dirname, "public")));
 app.use("/images", express.static(path.join(__dirname, "images")));
@@ -64,17 +68,12 @@ app.use(
     store: store,
   })
 );
+
 app.use(csrfProtection);
 app.use(flash());
 
 app.use((req, res, next) => {
-  res.locals.isAuthenticated = req.session.isLoggedIn;
-  res.locals.csrfToken = req.csrfToken();
-  next();
-});
-
-app.use((req, res, next) => {
-  // throw new Error('Sync Dummy');
+  // throw new Error("Sync Dummy");
   if (!req.session.user) {
     return next();
   }
@@ -91,6 +90,12 @@ app.use((req, res, next) => {
     });
 });
 
+app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.session.isLoggedIn;
+  res.locals.csrfToken = req.csrfToken();
+  next();
+});
+
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 app.use(authRoutes);
@@ -100,8 +105,7 @@ app.get("/500", errorController.get500);
 app.use(errorController.get404);
 
 app.use((error, req, res, next) => {
-  // res.status(error.httpStatusCode).render(...);
-  // res.redirect('/500');
+  // res.status(error.httpStatiusCode).render(...)
   res.status(500).render("500", {
     pageTitle: "Error!",
     path: "/500",
--- controllers/shop.js
+++ controllers/shop.js
@@ -169,7 +169,23 @@ exports.getInvoice = (req, res, next) => {
       pdfDoc.pipe(fs.createWriteStream(invoicePath));
       pdfDoc.pipe(res);
 
-      pdfDoc.text("Hello Zidane");
+      pdfDoc.fontSize(26).text("Invoice", {
+        underline: true,
+      });
+      pdfDoc.text("----------------------------------");
+      let totalPrice = 0;
+      order.products.forEach((prod) => {
+        totalPrice = totalPrice + prod.quantity * prod.product.price;
+        pdfDoc
+          .fontSize(20)
+          .text(
+            (prod.product.title =
+              " - " + prod.quantity + " x " + " $ " + prod.product.price)
+          );
+      });
+      pdfDoc.text("--------------------------");
+      pdfDoc.text("Total Price : $" + totalPrice);
+
       pdfDoc.end();
       // fs.readFile(invoicePath, (err, data) => {
       //   if (err) {
